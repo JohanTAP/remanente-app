e2eea227a6180ff4068a288f2b3c30d8e3c4e4a1
Johan Gutierrez <johgutierrez22@cftsa.cl>
2024-11-11 15:43:49 -0300

    corregir rutas de imagen en los periodos del componente PeriodosCarousel

--- app/page.tsx
+++ app/page.tsx
@@ -23,10 +23,10 @@ const periodos: Periodo[] = [
   { id: 7, nombre: 'Reino Dividido', descripcion: 'Desde los conflictos internos que dividieron la nación de Israel hasta los profetas que advirtieron sobre el exilio venidero.', imagen: '/periodo_7.webp' },
   { id: 8, nombre: 'El Exilio', descripcion: 'Desde la caída de Israel hasta el exilio en Babilonia y los grandes profetas declarando la venida del Mesías.', imagen: '/periodo_8.webp' },
   { id: 9, nombre: 'Vida de Cristo', descripcion: 'Desde el nacimiento del prometido Mesías, Jesús el Cristo, hasta su ministerio, muerte y resurrección.', imagen: '/periodo_9.webp' },
-  { id: 10, nombre: 'Iglesia Primitiva', descripcion: 'Desde el martirio de Esteban hasta la persecución y el auge de la iglesia a través de Pablo y los apóstoles.', imagen: '/periodo_9.webp' },
-  { id: 11, nombre: 'Edad Media', descripcion: 'Desde la legalización del cristianismo bajo Constantino hasta la persecución de la Edad Oscura.', imagen: '/9.webp' },
-  { id: 12, nombre: 'La Reforma', descripcion: 'Desde los grandes reformadores, como Lutero y Wycliffe, hasta la expansión del movimiento protestante.', imagen: '/9.webp' },
-  { id: 13, nombre: 'Profecías de Apocalipsis', descripcion: 'Desde el surgimiento del anticristo, la marca de la bestia, hasta el regreso de Cristo y la restauración del paraíso.', imagen: '/9.webp' }
+  { id: 10, nombre: 'Iglesia Primitiva', descripcion: 'Desde el martirio de Esteban hasta la persecución y el auge de la iglesia a través de Pablo y los apóstoles.', imagen: '/periodo_10.webp' },
+  { id: 11, nombre: 'Edad Media', descripcion: 'Desde la legalización del cristianismo bajo Constantino hasta la persecución de la Edad Oscura.', imagen: '/periodo_10.webp' },
+  { id: 12, nombre: 'La Reforma', descripcion: 'Desde los grandes reformadores, como Lutero y Wycliffe, hasta la expansión del movimiento protestante.', imagen: '/periodo_10.webp' },
+  { id: 13, nombre: 'Profecías de Apocalipsis', descripcion: 'Desde el surgimiento del anticristo, la marca de la bestia, hasta el regreso de Cristo y la restauración del paraíso.', imagen: '/periodo_10.webp' }
 ];
 
 export default function PeriodosCarousel ()
