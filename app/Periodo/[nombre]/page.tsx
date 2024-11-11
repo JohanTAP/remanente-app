d3e1475c654584227221d9f690900bca5d8148ef
Johan Gutierrez <johgutierrez22@cftsa.cl>
2024-11-11 18:23:41 -0300

    agregar título "Cronología" en la página de detalles del período

--- app/Periodo/[nombre]/page.tsx
+++ app/Periodo/[nombre]/page.tsx
@@ -29,6 +29,7 @@ export default function PeriodoPage ( { params }: { params: { nombre: string } }
 
     return (
         <div className="p-8">
+            <h1 className="text-4xl font-bold">Cronología</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
                 { periodo.Eventos.map( ( evento, index ) => (
                     <Card
