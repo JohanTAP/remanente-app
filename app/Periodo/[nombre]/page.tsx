f1fd6f3ef1dafb01a3312245016ad0d6d2e088e5
Johan Gutierrez <johgutierrez22@cftsa.cl>
2024-11-11 18:56:09 -0300

    refactor: simplificar la definición de tipos en PeriodoPage y optimizar la función generateStaticParams

--- app/Periodo/[nombre]/page.tsx
+++ app/Periodo/[nombre]/page.tsx
@@ -1,19 +1,16 @@
 import periodosData from '@/lib/periodos.json';
 import PeriodoContent from './PeriodoContent';
 
-interface PeriodoPageProps
-{
+type PeriodoPageProps = {
     params: { nombre: string };
+};
+
+export async function generateStaticParams() {
+    return Object.keys(periodosData.Periodos).map((nombre) => ({ nombre } as const));
 }
 
-export async function generateStaticParams ()
-{
-    return Object.keys( periodosData.Periodos ).map( ( nombre ) => ( { nombre } as const ) );
-}
+export default async function PeriodoPage({ params }: PeriodoPageProps) {
+    const nombre = await params.nombre;
 
-export default function PeriodoPage ( { params }: PeriodoPageProps )
-{
-    const nombre = params.nombre;
-
-    return <PeriodoContent nombre={ nombre } />;
+    return <PeriodoContent nombre={nombre} />;
 }
