1c95294b974187a89932fea746458450f36397d8
Johan Gutierrez <johgutierrez22@cftsa.cl>
2025-03-05 09:35:31 -0300

    feat: agregar componentes de interfaz de usuario y utilidades, incluyendo Skeleton, Separator, Collapsible, Input y Avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-export default function Home() {
+export default function Page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      </footer>
-    </div>
-  );
+    <main>
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <p>Remanente App</p>
+      </div>
+    </main>
+  )
 }
